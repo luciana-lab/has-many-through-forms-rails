916008503114a2632bbdcf3f092c6756185e1a70
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-06 17:35:49 -0400

    commit tree 2c980afce11ed65d7beb1ea020aebc42c9a7d5ae

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  #accepts_nested_attributes_for :categories
 
-
+  #only creating a new category if it doesn't already exist with the current name
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
